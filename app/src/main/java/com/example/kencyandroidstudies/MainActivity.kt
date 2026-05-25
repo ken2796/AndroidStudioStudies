141bd2586a17deca07ef1998618587c41e8576f8
Kenneth Francia <81939090+ken2796@users.noreply.github.com>
2026-05-26 01:40:16 +0800

    Center the View

--- app/src/main/java/com/example/kencyandroidstudies/MainActivity.kt
+++ app/src/main/java/com/example/kencyandroidstudies/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kencyandroidstudies.cyan.ImageCyanViewPreview
@@ -28,7 +32,11 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun MainStudyProject() {
-    Column {
+    Column(modifier = Modifier
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         MainKenView()
         MainCyanView()
     }
