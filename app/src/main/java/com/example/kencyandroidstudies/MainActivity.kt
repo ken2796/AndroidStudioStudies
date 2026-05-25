9f0b9729233a576df791e476cc711a6ced1211f8
Kenneth Francia <81939090+ken2796@users.noreply.github.com>
2026-05-26 01:21:38 +0800

    Add view on main activity ImageKenView

--- app/src/main/java/com/example/kencyandroidstudies/MainActivity.kt
+++ app/src/main/java/com/example/kencyandroidstudies/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.kencyandroidstudies.ken.ImageKenViewPreview
 import com.example.kencyandroidstudies.ui.theme.KenCyAndroidStudiesTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +20,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            KenCyAndroidStudiesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainStudyProject()
+
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainStudyProject() {
+    Column {
+        MainKenView()
+    }
+}
+@Composable
+fun MainKenView() {
+    ImageKenViewPreview()
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     KenCyAndroidStudiesTheme {
-        Greeting("Android")
+        MainStudyProject()
     }
 }
